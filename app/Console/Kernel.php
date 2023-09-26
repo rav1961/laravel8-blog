6bae0da2dd429cca8137c8cc2c84c3cb91e666c9
rav1961 <r.rojek87@gmail.com>
2023-09-26 07:37:28 +0200

    install larastan

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
     }
 
     /**
@@ -26,7 +25,5 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 }
