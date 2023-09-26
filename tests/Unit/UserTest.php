c49c10a4faa20454b6eeb88492b905a8c355469e
rav1961 <r.rojek87@gmail.com>
2023-09-26 15:19:12 +0200

    add more User tests

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Enums\Role;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
@@ -16,5 +20,42 @@ class UserTest extends TestCase
             'id', 'first_name', 'email', 'email_verified_at',
             'password', 'role', 'created_at', 'updated_at',
         ]));
+    }
+
+    public function test_has_fillable_attributes(): void
+    {
+        $fillable = ['first_name', 'email', 'password', 'role'];
+
+        $user = User::factory()->create();
+
+        $this->assertEquals($fillable, $user->getFillable());
+    }
+
+    public function test_has_hidden_attributes(): void
+    {
+        $hidden = ['password', 'remember_token'];
+
+        $user = User::factory()->create();
+
+        $this->assertEquals($hidden, $user->getHidden());
+    }
+
+    public function test_it_cast_role_to_enum(): void
+    {
+        $user = User::factory()->create([
+            'role' => Role::ADMIN,
+        ]);
+
+        $this->assertInstanceOf(Role::class, $user->role);
+        $this->assertEquals(Role::ADMIN, $user->role);
+    }
+
+    public function test_user_has_many_posts(): void
+    {
+        $user = User::factory()->hasPosts(3)->create();
+
+        $this->assertCount(3, $user->posts);
+        $this->assertInstanceOf(Collection::class, $user->posts);
+        $this->assertContainsOnly(Post::class, $user->posts);
     }
 }
